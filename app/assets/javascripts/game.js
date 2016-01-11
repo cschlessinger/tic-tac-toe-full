69ec49b755402178d9799b6c3e4661ff1c1d1ce5
cschlessinger <claire.schlessinger@gmail.com>
2016-01-11 15:55:43 -0800

    All win types working

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -1,68 +1,85 @@
 $(document).ready(function() {
-	// var x = "x";
-	// var	o = "o";
-	var gameOver = false;
 	var player = "o";
 	var	moves = 0;
 	var grid = $('.square');
-	$('.square').on("click", function() {
-		var gridContents = [];
+	var winner;
+	var play = function() {
+		$('.square').on("click", function() {
+			var gridContents = [];
 
-		var rowWin = function() {
-			for (var i=0, group=3; i<gridContents.length; i+=3) {
-				if (gridContents.slice(i, i+group).every(elem => elem == player)) {
-					console.log(player + " win");
+			if (validMove($(this))) {
+
+				$(this).html(player);
+				$(this).addClass(player);
+
+				grid.each(function() {
+					gridContents.push($(this).html());
+				})
+
+				if (columnWin(gridContents, player) != undefined) {
+					winner = columnWin(gridContents, player)
 				}
-			}
-		};
-
-		var columnWin = function() {
-			var columnArray=[]
-			for (var i=0; i<3; i++) {
-				columnArray.push([gridContents[i], gridContents[i+3], gridContents[i+6]]);
-			}
-			for (var column in columnArray) {
-				if (columnArray[column].every(elem => elem == player)) {
-					console.log(player + " win")
+				else if (rowWin(gridContents, player) != undefined) {
+					winner = rowWin(gridContents, player)
 				}
-			}
-		};
+				else if (leftRightWin(gridContents, player) != undefined) {
+					winner = leftRightWin(gridContents, player)
+				}
+				else if (rightLeftWin(gridContents, player) != undefined) {
+					winner = rightLeftWin(gridContents, player)
+				}
 
-		var diagonalWin = function() {
+				if (winner != undefined) {
+					alert(winner);
+				}
 
-		}
-
-		if (validMove($(this))) {
-
-			// Update x/o on screen, in classes, and in current player variable
-			$(this).html(player);
-			$(this).addClass(player);
-
-			grid.each(function() {
-				gridContents.push($(this).html());
-			})
-
-			columnWin();
-			rowWin();
-
-			if (gameOver == true || moves == 9) {
-				endGame();
+				player = player == 'x' ? 'o' : 'x';
+				moves += 1;
 			}
 
-			player = player == 'x' ? 'o' : 'x';
-			// checkWin();
-			moves += 1;
-		}
-
-	})
-
+		})
+	}
+	play();
 })
 
+var rowWin = function(array, p) {
+	for (var i=0, group=3; i<array.length; i+=3) {
+		if (array.slice(i, i+group).every(elem => elem == p)) {
+			return p;
+		}
+	};
+};
 
+var columnWin = function(array, p) {
+	var columnArray=[];
+	for (var i=0; i<3; i++) {
+		columnArray.push([array[i], array[i+3], array[i+6]]);
+	};
+	for (var column in columnArray) {
+		if (columnArray[column].every(elem => elem == p)) {
+			return p;
+		}
+	};
+};
 
-var checkWin = function() {
-	rowWin();
-	columnWin();
+var leftRightWin = function(array, p) {
+	var diagonalArray=[];
+	for (var i=0; i<array.length; i+=4) {
+		diagonalArray.push(array[i]);
+		if (!diagonalArray.includes("") && diagonalArray.length == 3 && diagonalArray.every(elem => elem == p)) {
+			return p;
+		}
+	}
+}
+
+var rightLeftWin = function(array, p) {
+	var diagonalArray=[];
+	for (var i=2; i<7; i+=2) {
+		diagonalArray.push(array[i]);
+		if (!diagonalArray.includes("") && diagonalArray.length == 3 && diagonalArray.every(elem => elem == p)) {
+			return p;
+		}
+	}
 }
 
 var validMove = function(square) {
@@ -75,16 +92,16 @@ var validMove = function(square) {
 	}
 }
 
-var reset = function() {
-	$('square').removeClass(x);
-	$('square').removeClass(o);
-	moves = 0;
-	alert("Game over!")
-}
+// var reset = function() {
+// 	$('square').removeClass("x");
+// 	$('square').removeClass("o");
+// 	moves = 0;
+// 	alert("Game over!")
+// }
 
-var endGame = function() {
-	reset();
-}
+// var endGame = function() {
+	// reset();
+// }
 
 // Make alert that game is over and start new game
 // Account for ties
