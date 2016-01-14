5d709a6e8fc309d08a229888c26d2e00221d5cf6
cschlessinger <claire.schlessinger@gmail.com>
2016-01-13 18:16:01 -0800

    Heroku changes

--- tic-tac-toe/app/assets/javascripts/game.js
+++ tic-tac-toe/app/assets/javascripts/game.js
@@ -1,28 +1,122 @@
 $(document).ready(function() {
-	var x = "x";
-	var	o = "o";
+	$('#winner').hide();
+	$('#reset').hide();
+	play();
+	reset();
+});
+
+var play = function() {
 	var player = "o";
 	var	moves = 0;
-	// var wins: [["one", "two", "three"], ["four", "five", "six"], ["seven"]]
-	$('.square').click(function() {
-		if ($(this).html() === "") { // Only record moves made on empty squares
-			$(this).html(player); // Add symbol to square on screen
-			$(this).addClass(player); // Record owner in class
-			if (player == o) {
-				player = x; // Toggle player player
-			}
-			else {
-				player = o;
-			};
-			moves += 1;
-			console.log(player);
-			console.log(moves);
-		}
-		else {
-			alert("Oops! Space is already taken");
-		}
-	})
-})
+	var grid = $('.square');
+	var winner;
+	$('.square').on("click", function() {
+		var gridContents = [];
 
-// Make alert that game is over and start new game
-// Account for ties
+		if (validMove($(this))) {
+
+			$(this).html(player);
+			$(this).addClass(player);
+
+			grid.each(function() {
+				gridContents.push($(this).html());
+			})
+
+			if (columnWin(gridContents, player) != undefined) {
+				winner = columnWin(gridContents, player)
+			}
+			else if (rowWin(gridContents, player) != undefined) {
+				winner = rowWin(gridContents, player)
+			}
+			else if (leftRightWin(gridContents, player) != undefined) {
+				winner = leftRightWin(gridContents, player)
+			}
+			else if (rightLeftWin(gridContents, player) != undefined) {
+				winner = rightLeftWin(gridContents, player)
+			}
+
+			player = player == "x" ? "o" : "x";
+			moves += 1;
+
+			if (moves < 10 && winner != undefined) {
+				if (winner == "x") {
+					$('#winner').addClass("x-background");
+				}
+				else {
+					$('#winner').addClass("o-background");
+				};
+				$('#winner').show().html(winner.toUpperCase() + " wins the game!");
+				$('#reset').show().html("Play again");
+				$('.square').off("click");
+			}
+			else if (moves == 9 && winner == undefined) {
+				$('#winner').addClass("tie-background").show().html("It's a tie!");
+				$('#reset').show().html("Play again");
+				$('.square').off("click");
+			};
+
+		}
+
+	})
+};
+
+var rowWin = function(array, p) {
+	for (var i=0, group=3; i<array.length; i+=3) {
+		if (array.slice(i, i+group).every(elem => elem == p)) {
+			return p;
+		}
+	};
+};
+
+var columnWin = function(array, p) {
+	var columnArray=[];
+	for (var i=0; i<3; i++) {
+		columnArray.push([array[i], array[i+3], array[i+6]]);
+	};
+	for (var column in columnArray) {
+		if (columnArray[column].every(elem => elem == p)) {
+			return p;
+		}
+	};
+};
+
+var leftRightWin = function(array, p) {
+	var diagonalArray=[];
+	for (var i=0; i<array.length; i+=4) {
+		diagonalArray.push(array[i]);
+		if (!diagonalArray.includes("") && diagonalArray.length == 3 && diagonalArray.every(elem => elem == p)) {
+			return p;
+		}
+	}
+}
+
+var rightLeftWin = function(array, p) {
+	var diagonalArray=[];
+	for (var i=2; i<7; i+=2) {
+		diagonalArray.push(array[i]);
+		if (!diagonalArray.includes("") && diagonalArray.length == 3 && diagonalArray.every(elem => elem == p)) {
+			return p;
+		}
+	}
+}
+
+var validMove = function(square) {
+	if (square.html() === "") {
+		return true;
+	}
+	else {
+		alert("Oops! Space is already taken");
+		return false;
+	}
+}
+
+var reset = function() {
+	$('#reset').on("click", function() {
+		$('#winner').hide();
+		$('#reset').hide();
+		$('.square').removeClass("x");
+		$('.square').removeClass("o");
+		$('.square').empty();
+		play();
+	})
+}
